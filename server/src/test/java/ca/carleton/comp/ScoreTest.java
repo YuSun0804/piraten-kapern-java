b1f50bf7fef4f5cf2d942970f7cafb2620f81be7
yusun <yusun@example.com>
2020-09-29 13:37:03 -0400

    TEST add compute winner tests

---
+++ server/src/test/java/ca/carleton/comp/ScoreTest.java
@@ -0,0 +1,49 @@
+package ca.carleton.comp;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ScoreTest {
+
+    private ScorePad scorePad = new ScorePad();
+
+    @Test
+    public void computeWinner1() {
+        ScoreRecord scoreRecord1 = new ScoreRecord(1, "p1", 0, -100);
+        ScoreRecord scoreRecord2 = new ScoreRecord(2, "p2", 100, 0);
+        ScoreRecord scoreRecord3 = new ScoreRecord(3, "p3", 6000, 0);
+        scorePad.addScoreRecord(scoreRecord1);
+        scorePad.addScoreRecord(scoreRecord2);
+        scorePad.addScoreRecord(scoreRecord3);
+        ScoreRecord winner = scorePad.computeWinner();
+        assertEquals(3, winner.getPlayerIndex());
+        assertEquals(5900, winner.getFinalScore());
+
+        int score = scorePad.computeScore(2);
+        assertEquals(0, score);
+    }
+
+    @Test
+    public void computeWinner2() {
+        ScoreRecord scoreRecord1 = new ScoreRecord(1, "p1", 0, -100);
+        ScoreRecord scoreRecord2 = new ScoreRecord(2, "p2", 0, -1000);
+        ScoreRecord scoreRecord3 = new ScoreRecord(3, "p3", 6000, 0);
+        ScoreRecord scoreRecord4 = new ScoreRecord(4, "p4", 3000, 0);
+
+        scorePad.addScoreRecord(scoreRecord1);
+        scorePad.addScoreRecord(scoreRecord2);
+        scorePad.addScoreRecord(scoreRecord3);
+        scorePad.addScoreRecord(scoreRecord4);
+
+        ScoreRecord winner = scorePad.computeWinner();
+        assertEquals(3, winner.getPlayerIndex());
+        assertEquals(4900, winner.getFinalScore());
+
+        int score = scorePad.computeScore(4);
+        assertEquals(1900, score);
+    }
+
+}
