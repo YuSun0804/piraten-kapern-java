ec2d98b0dd9d15c433ec983a3e7dd002ed1e415a
yusun <yusun@example.com>
2020-09-29 10:38:49 -0400

    TEST add test suite

---
+++ client/src/test/java/ca/carleton/comp/TestAll.java
@@ -0,0 +1,13 @@
+package ca.carleton.comp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        Part1Test.class,
+        Part2Test.class,
+})
+public class TestAll {
+
+}
